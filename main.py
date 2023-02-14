f3b00578070fcae1ad966c970335ca05868ea87b
BWPipeline <pbr.9816@gmail.com>
2023-02-14 12:04:49 -0500

    Versión actual

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ db = DBManager()
 
 bw.login()
 date = Utils.today()
+group_name = 'Banco de Bogotá'
+df = bw.download_group_data_to_df(group_name, date, Utils.get_next_day(date))
+df = Utils.get_summary_bdb(df, Utils.get_dia(date), Utils.get_mes(date), Utils.get_año(date))
+Utils.charge_sql_bdb(db, df)
+
+query_name = 'Competidores (Banco de Bogotá) 2023'
+df = bw.download_query_data_to_df(query_name, date, Utils.get_next_day(date))
+df = Utils.get_summary_comp(df, Utils.get_dia(date), Utils.get_mes(date), Utils.get_año(date))
+Utils.charge_sql_comp(db, df)
+date = Utils.get_next_day(date)
+
 
 # cargar histórico
 # while Utils.active_date(date):
